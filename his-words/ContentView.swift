ce632aabe3877985db1a74be823c03fc0b830840
Weston Clark <westoclark@gmail.com>
2026-04-17 12:54:09 -0700

    Initial Commit

---
+++ his-words/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  his-words
+//
+//  Created by Weston Clark on 4/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ his-words/his_wordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  his_wordsApp.swift
+//  his-words
+//
+//  Created by Weston Clark on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct his_wordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
